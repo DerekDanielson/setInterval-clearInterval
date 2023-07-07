fed41b52af3f7e9c992059926f911bca8ceddcbd
Derek <derekdanielson84@gmail.com>
2023-07-07 09:23:24 -0600

    Notes added

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
-const intervalID = setInterval(myCallback, 1000);
+//const intervalID = setInterval(myCallback, 1000, 'Hello');
 
-function myCallback(){
-    console.log(Date.now());
+//function myCallback(){
+//    console.log(a, Date.now());
+//}
+
+let intervalID;
+
+function startChange(){
+    if (!intervalID){
+        intervalID = setInterval(changeColor, 1000);
+    }
 }
 
+function changeColor(){
+    if (document.body.style.backgroundColor !== 'black'){
+        document.body.style.backgroundColor = 'black';
+        document.body.style.color = 'white';
+    } else {
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = 'black';
+    }
+}
 
 function stopChange(){
     clearInterval(intervalID);
 }
 
+document.getElementById('start').addEventListener('click', startChange);
 document.getElementById('stop').addEventListener('click', stopChange);
